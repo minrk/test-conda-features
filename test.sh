ec9a56c245f6d00701d5d92a86f169458d239bb3
Min RK <benjaminrk@gmail.com>
2017-02-21 16:42:41 +0100

    solve upgrade --all by tracking features in providers

--- test.sh
+++ test.sh
@@ -4,24 +4,28 @@ tmpdir=$(mktemp -d)
 ARGS="-yq --use-local"
 
 set -ex
-# should get y + consumer
+# should get preferred + consumer
 conda create $ARGS -p $tmpdir/test-consumer consumer
 # should get x + consumer
 conda create $ARGS -p $tmpdir/test-x providerx consumer
 # should get y + consumer
 conda create $ARGS -p $tmpdir/test-y providery consumer
 
-# should get x
-conda create $ARGS -p $tmpdir/test-2-step providerx
-# should stick with x
-conda install $ARGS -p $tmpdir/test-2-step consumer
+# should get and stick with y
+conda create $ARGS -p $tmpdir/test-y-2-step providery
+conda install $ARGS -p $tmpdir/test-y-2-step consumer
+# should get and stick with x
+conda create $ARGS -p $tmpdir/test-x-2-step providerx
+conda install $ARGS -p $tmpdir/test-x-2-step consumer
 
-# should get x + consumer (ok)
-conda create $ARGS -p $tmpdir/test-upgrade-all providerx consumer
-# should stick with x (doesn't: downgrades providerx-x_1 to providerx-0)
-conda upgrade --all -p $tmpdir/test-upgrade-all $ARGS
+# should get and stick with x
+conda create $ARGS -p $tmpdir/test-x-upgrade-all providerx consumer
+conda upgrade --all -p $tmpdir/test-x-upgrade-all $ARGS
+# should get and stick with y
+conda create $ARGS -p $tmpdir/test-y-upgrade-all providery consumer
+conda upgrade --all -p $tmpdir/test-y-upgrade-all $ARGS
 
 set +x
-for name in `ls "$tmpdir"`; do
+for name in `ls "$tmpdir" | sort`; do
     conda list -p "$tmpdir/$name"
 done
