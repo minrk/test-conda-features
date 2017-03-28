a0e210d0b97cb3a9e260d08e8bfababc3f94847d
Min RK <benjaminrk@gmail.com>
2017-03-28 15:51:57 +0200

    add tests for no-provider case

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ tmpdir=$(mktemp -d)
 ARGS="-yq --use-local"
 
 set -ex
-# should get preferred + consumer
+# should get just consumer (no provider)
 conda create $ARGS -p $tmpdir/test-consumer consumer
 # should get x + consumer
 conda create $ARGS -p $tmpdir/test-x providerx consumer
@@ -18,9 +18,24 @@ conda install $ARGS -p $tmpdir/test-y-2-step consumer
 conda create $ARGS -p $tmpdir/test-x-2-step providerx
 conda install $ARGS -p $tmpdir/test-x-2-step consumer
 
+
+# should start with no consumer, then switch to x when provider is installed (works)
+conda create $ARGS -p $tmpdir/test-no-then-x consumer
+conda install $ARGS -p $tmpdir/test-no-then-x providerx
+
+# should start with x, then switch to y when providery is installed (doesn't because x and y don't conflict)
+conda create $ARGS -p $tmpdir/test-x-then-y providerx consumer
+conda install $ARGS -p $tmpdir/test-x-then-y providery
+
+
+# should get and stick with no-provider
+conda create $ARGS -p $tmpdir/test-no-upgrade-all consumer
+conda upgrade --all -p $tmpdir/test-no-upgrade-all $ARGS
+
 # should get and stick with x
 conda create $ARGS -p $tmpdir/test-x-upgrade-all providerx consumer
 conda upgrade --all -p $tmpdir/test-x-upgrade-all $ARGS
+
 # should get and stick with y
 conda create $ARGS -p $tmpdir/test-y-upgrade-all providery consumer
 conda upgrade --all -p $tmpdir/test-y-upgrade-all $ARGS
